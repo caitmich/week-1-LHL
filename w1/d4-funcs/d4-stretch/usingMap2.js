04d31e1b9db36d657fcfa5bbcfb8e02ca46ff9be
Caitlin Henry <caitlinhenry.ss@gmail.com>
2021-08-19 20:51:47 -0400

    using the map function again

---
+++ w1/d4-funcs/d4-stretch/usingMap2.js
@@ -0,0 +1,14 @@
+const input = [
+  { x: 3, y: 4 },
+  { x: 12, y: 5 },
+  { x: 8, y: 15 }
+];
+const result = input.map(z => {
+  const pyth = Math.sqrt(((z.x)*(z.x)) + ((z.y)*(z.y)));
+  return pyth;
+});
+console.log(result);
+
+console.log(result[0] === 5);
+console.log(result[1] === 13);
+console.log(result[2] === 17);
